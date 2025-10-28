353e85516988cbbdca315efa94f054378df87f08
Lucca Gabriel <luccagabriel12@hotmail.com>
2025-10-28 02:38:05 -0300

    ..

--- src/features/real_estate/controllers/real_estate_controller.js
+++ src/features/real_estate/controllers/real_estate_controller.js
@@ -4,113 +4,27 @@ import RealEstate from "../models/real_estate.js";
 import mongoose from "mongoose";
 
 export default {
-  get: async (req, res, next) => {
+  get: async (req, res) => {
     try {
       const realEstateList = await RealEstate.find({});
-      // const realEstateList = [
-      //   {
-      //     _id: "1",
-      //     title: "Casa",
-      //     description: "Casa em boa condição",
-      //     type: "house",
-      //     price: 1000000,
-      //     area: 100,
-      //     rooms: 4,
-      //     bathrooms: 2,
-      //     garages: 1,
-      //     thumbnail: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLwsfo9NC1pzBgR2nmp8LIBDIPbZSqODx4Sg&s",
-      //     images: [],
-      //     address: {
-      //       _id: "4",
-      //       targetId: "1",
-      //       cep: "12345-678",
-      //       street: "Street",
-      //       district: "District",
-      //       city: "City",
-      //       state: "State",
-      //       complement: "Complement",
-      //       number: "Number",
-      //       position: {
-      //         lat: -24.968731,
-      //         lng: -53.519697,
-      //       },
-      //     },
-      //   },
-      //   {
-      //     _id: "2",
-      //     title: "Casa",
-      //     description: "Casa em boa condição",
-      //     type: "apartament",
-      //     price: 2000000,
-      //     area: 200,
-      //     rooms: 3,
-      //     bathrooms: 1,
-      //     garages: 1,
-      //     thumbnail: "https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg",
-      //     images: [],
-      //     address: {
-      //       _id: "4",
-      //       targetId: "1",
-      //       cep: "12345-678",
-      //       street: "Street",
-      //       district: "District",
-      //       city: "City",
-      //       state: "State",
-      //       complement: "Complement",
-      //       number: "Number",
-      //       position: {
-      //         lat: -24.960731,
-      //         lng: -53.519697,
-      //       },
-      //     },
-      //   },
-      // ];
 
       return res.json({ status: 200, payload: realEstateList, message: "Ok!" });
     } catch (error) {
       return res.json({ status: 500, message: JSON.stringify(error) });
     }
   },
-  getById: async (req, res, next) => {
+  getById: async (req, res) => {
     const { _id } = req.params;
 
     try {
       const realEstate = await RealEstate.findOne({ _id });
-      // const realEstate = {
-      //   _id: "2",
-      //   title: "Casa",
-      //   description: "Casa em boa condição",
-      //   type: "apartament",
-      //   price: 2000000,
-      //   area: 200,
-      //   rooms: 3,
-      //   bathrooms: 1,
-      //   garages: 1,
-      //   thumbnail: "https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg",
-      //   images: [],
-      //   address: {
-      //     _id: "4",
-      //     targetId: "1",
-      //     cep: "12345-678",
-      //     street: "Street",
-      //     district: "District",
-      //     city: "City",
-      //     state: "State",
-      //     complement: "Complement",
-      //     number: "Number",
-      //     position: {
-      //       lat: -24.960731,
-      //       lng: -53.519697,
-      //     },
-      //   },
-      // };
 
       return res.json({ status: 200, payload: realEstate, message: "Ok!" });
     } catch (error) {
       return res.json({ status: 500, message: JSON.stringify(error) });
     }
   },
-  create: async (req, res, next) => {
+  create: async (req, res) => {
     try {
       const realEstate = await RealEstate.create(req.body);
 
@@ -119,7 +33,7 @@ export default {
       return res.json({ status: 500, message: JSON.stringify(error) });
     }
   },
-  update: async (req, res, next) => {
+  update: async (req, res) => {
     const { _id } = req.body;
 
     try {
@@ -131,7 +45,7 @@ export default {
     }
   },
   //
-  importOldDB: async (req, res, next) => {
+  importOldDB: async (req, res) => {
     try {
       console.log("Importing Old DB...");
       const oldDB = mongoose.createConnection("mongodb+srv://admin:dev@cluster0.2agfw.mongodb.net/?appName=Cluster0", {
